80a48c4863602e15a26d30342177f29d5992a474
wanziee <@ichwanalghifa@gmail.com>
2024-12-26 21:27:16 +0700

    Initial Commit

---
+++ belajar swift/belajar_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  belajar_swiftApp.swift
+//  belajar swift
+//
+//  Created by mohammad ichwan al ghifari on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct belajar_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
